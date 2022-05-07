9ae7801d033a5092346abff25748b6c552d39fa7
kuaio <bestvpn7@gmail.com>
2022-05-07 17:39:50 +0800

    last update

---
+++ docs/passwd.js
@@ -0,0 +1,51 @@
+var password=document.getElementById("password");
+
+function genPassword() {
+    var chars = "23456789abcdefghkmnpqrstwxyzABCDEFGHKMNPRSTWXYZ+@#$";
+    var passwordLength = 18;
+    var password = "";
+    for (var i = 1; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 999);
+    document.execCommand("copy");
+}
+
+function getid(i) {
+	var url = "https://www.sop.pw/app/id.json"            				// 申明一个XMLHttpRequest
+	var request = new XMLHttpRequest();             		// 设置请求方法与路径
+	request.open("get", url);	                		// 不发送数据到服务器
+	request.send(null);						//XHR对象获取到返回信息后执行
+	request.onload = function () {                  		// 返回状态为200，即为数据获取成功
+		if (request.status == 200) {
+			var data = JSON.parse(request.responseText);
+			console.log(data);                    		//获取jsonTip的div
+			var $jsontip = $("#jsonTip");          		//存储数据的变量 
+			var strHtml = "";                       	//清空内容 
+			$jsontip.empty();                       	//将获取到的json格式数据遍历到div中
+			var arr = Object.keys(data);
+			console.log(arr.length);
+
+                        if(i == 0) {	strHtml += "账号: " + data[i].id + "<br><br>";
+                        strHtml += "密码: " + data[i].pwd + "<br><br>"; 
+			strHtml += '登录时，不要开启"双重认证" [<a href="javascript:getid(1)">备用账号</a>]';	}
+
+			else {	strHtml += "<font color=darkred>账号: " + data[1].id + "<br><br>";
+				strHtml += "密码: " + data[1].pwd + "<br></font>";	
+				if(arr.length > 2) {	
+					strHtml += "<br><font color=red>账号: " + data[2].id + "<br><br>";
+					strHtml += "密码: " + data[2].pwd + "<br></font>";	}
+			}
+
+			$jsontip.html(strHtml);
+
+		}
+	}
+}
---
+++ docs/pwd.md
@@ -0,0 +1,75 @@
+<style type="text/css"> 
+.box{
+	background-color: white;
+}
+
+.box h2{
+  margin-bottom: 30px;
+  text-align: left;
+  font-size: 26px;
+  color: #015a96;
+  font-family: sans-serif;
+}
+
+.box table{
+  width: 300px;
+  align-items: center;
+  border: 0px solid;
+}
+
+input {
+  padding: 20px;
+  user-select: none;
+  height: 50px;
+  width: 300px;
+  border-radius: 6px;
+  border: none;
+  border: 2px solid rgb(13, 152, 245);
+  outline: none;
+  font-size: 22px;
+}
+
+input::placeholder{
+  font-size: 23px;
+} 
+
+#button {
+	font-family: sans-serif;
+	font-size: 15px;
+	border: 2px solid rgb(20, 139, 250);
+	width: 121px;
+	height: 50px;
+	text-align: center;
+	background-color: #0c81ee;
+	display: flex;
+	color: rgb(255, 255, 255);
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+	border-radius: 7px;
+}
+
+#button:hover {
+	color: white;
+	background-color: black;
+}
+</style> 
+
+## 改SS密码
+
+*  登入您购买`SS节点`的网站，如`账号`的`SS密码`不符合安全要求，请点击`续费`旁的`改SS密码`
+
+![windows](media/win/pwd.jpg ':size=640')
+
+<div class="box">
+<h2>随机密码生成器</h2>
+<input type="text" name="" placeholder=" 随机密码" id="password" readonly><br /><br />
+<table>
+ <tr style="border: none;">
+   <th style="border: none;"><div id="button" class="btn1" onclick="genPassword()">生成</div></th>
+   <th style="border: none;"><div id="button" class="btn2" onclick="copyPassword()">复制</div></th>
+ </tr>
+</table>
+</div>
+
+!> 改`SS密码`后，请删掉客户端里的路线，重新添加。`如何删除路线？`[点击这里](other?id=删除路线)
